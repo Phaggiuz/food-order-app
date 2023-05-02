50643dcf02fa1a3dfb6c8563c95040451cb38d3b
Phaggiuz <francesco.faggioni.1994@gmail.com>
2023-05-02 10:52:57 +0200

    fixing MealtemForm

--- src/Components/Meals/MealItem/MealItemForm.js
+++ src/Components/Meals/MealItem/MealItemForm.js
@@ -7,7 +7,7 @@ const MealItemForm = (props) => {
       <Input
         label="Amount"
         input={{
-          id: "amount",
+          id: "amount" + props.id,
           type: "bnumber",
           min: "1",
           max: "5",
